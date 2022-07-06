5146a1cf8e87f976c5e75c0550f4d1d5fb0b3638
Alicastic <alicastic@Alicastics-MacBook-Pro.local>
2022-07-06 14:29:56 +0700

    make api

--- api.py
+++ api.py
@@ -1,3 +1,6 @@
+from pickle import TRUE
+import resource
+from unicodedata import name
 from flask import Flask, request, jsonify, make_response
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
@@ -7,7 +10,7 @@ app = Flask(__name__)
 #create API object
 api = Api(app)
 #create database object
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///thu.db'
 app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #sqlarchemy mapper
 db = SQLAlchemy(app)
@@ -22,6 +25,76 @@ class Employee(db.Model):
 
     def __repr__(self):
         return f"{self.firstname} - {self.lastname} - {self.gender} - {self.salary}"
+
+#For GET request to http://localhost:5000
+class getEmployee(Resource):
+    def get(self):
+        employee=Employee.query.all()
+        emp_list = []
+        for emp in employee:
+            emp_detail = {'id':emp.id,
+            'firstname':emp.firstname,
+            'lastname':emp.lastname,
+            'gender':emp.gender, 
+            'salary':emp.salary}
+            emp_list.append(emp_detail)
+        return {"Employees":emp_list},200
+
+#For POST request to http://localhost:5000/employee
+class AddEmployee (Resource):
+    def post(self):
+        if request.is_json:
+            emp = Employee(firstname=request.json['firstname'], 
+            lastname=request.json['lastname'],
+            gender=request.json['gender'],
+            salary=request.json['salary'])
+            db.session.add()
+            db.session.commit()
+            #return json response
+            return make_response(jsonify({
+                'id':emp.id,
+                'firstname':emp.firstname,
+                'lastname':emp.lastname,
+                'gender':emp.gender, 
+                'salary':emp.salary}), 201)
+        else:
+            return {'Error':'Request must be Json'},400
+
+#For PUT request to http://localhost:5000/update/?
+class UpdateEmployee (Resource):
+    def put(self,id):
+        if request.is_json:
+            emp = Employee.query.get(id)
+            if emp is None:
+                return {'Error':'Employee not found'},404
+            else:
+                emp.firstname = request.json['firstname']
+                emp.lastname = request.json['lastname']
+                emp.gender = request.json['gender']
+                emp.salary = request.json['salary']
+                db.session.commit()
+                return 'Updated', 200
+        else:
+            return {'Error':'Not found'}, 404
+
+#For delete request to http://localhost:5000/delete/?
+class DeleteEmployee (Resource):
+    def delete(self,id):
+        emp = Employee.query.get(id)
+        if emp is None:
+            return {'Error':'Employee not found'},404
+        db.session.delete(emp)
+        db.session.commit()
+        return f'{id} is deleted', 200
+
+api.add(getEmployee, '/')
+api.add(AddEmployee, '/add')
+api.add(UpdateEmployee, '/update/<int:id>')
+api.add(DeleteEmployee, '/delete/<int:id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
 
